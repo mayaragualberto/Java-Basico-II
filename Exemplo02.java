630d2b04e8d5679682f41f2d32b00504d531afca
Mayara Gualberto <64241581+mayaragualberto@users.noreply.github.com>
2021-03-10 21:44:43 -0300

    Add files via upload

---
+++ Exemplo02.java
@@ -0,0 +1,47 @@
+public class Exemplo02 {
+
+      public static void main(String[] args) {
+
+          double N1, N2, N3, N4, RES;
+
+         
+
+          System.out.print("Informe a nota do primeiro bimestre: ");
+
+          N1 = Double.parseDouble(System.console().readLine());
+
+          System.out.print("Informe a nota do segundo bimestre: ");
+
+          N2 = Double.parseDouble(System.console().readLine());
+
+          System.out.print("Informe a nota do terceiro bimestre: ");
+
+          N3 = Double.parseDouble(System.console().readLine());
+
+          System.out.print("Informe a nota do quarto bimestre: ");
+
+          N4 = Double.parseDouble(System.console().readLine());
+
+         
+
+          RES = (N1 + N2 + N3 + N4) / 4;
+
+          System.out.printf("Media das notas = %.1f\n", RES);
+
+         
+
+          if(RES >= 7){
+
+                System.out.println("Aluno aprovado!");
+
+                System.out.println("Parabens!");}
+
+          else{
+
+                System.out.println("Aluno reprovado!");
+
+                System.out.println("Voce deve repetir a materia.");}
+
+     }
+
+}
